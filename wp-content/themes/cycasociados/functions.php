33699b9a5f35fad7397da07372e883aeb7ae645a
Krobing <linkrobin1024@gmail.com>
2015-12-16 04:58:04 -0500

    preparación entorno

--- wp-content/themes/cycasociados/functions.php
+++ wp-content/themes/cycasociados/functions.php
@@ -6,11 +6,11 @@
  */
 
 // get styles of parent theme
-/*function child_enqueue_styles() {
+function child_enqueue_styles() {
     $parent_style = 'parent-style';
     wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
 }
-add_action( 'wp_enqueue_scripts', 'child_enqueue_styles',99);*/
+add_action( 'wp_enqueue_scripts', 'child_enqueue_styles',99);
 
 if ( get_stylesheet() !== get_template() ) {
     add_filter( 'pre_update_option_theme_mods_' . get_stylesheet(), function ( $value, $old_value ) {
